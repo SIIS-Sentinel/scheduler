c568b2646f34b08d3f219f4b7989a6f15b0984ed
AdrienCos <ad.cosson@gmail.com>
2020-05-07 12:56:39 -0400

    Broken implementation of the periodic_change

--- src/trace_maker.py
+++ src/trace_maker.py
@@ -1,10 +1,11 @@
 from src.full_trace import Trace
 from src.schedule_types import State, ScheduleElement, schedule_from_dict
-from typing import List
+from typing import List, Dict, Tuple
 
 import os
 import json
 import random
+import math
 
 
 class TraceMaker():
@@ -69,7 +70,64 @@ class TraceMaker():
                             max_duration = end_time - time
 
             elif element.elem_type == "periodic_change":
-                pass
+                events_number = math.ceil((self._duration + 1440) / element.update_period)
+                states_dict: Dict[int, int] = {}
+                events_list: List[Tuple[int, str]] = []
+                for i in range(events_number):
+                    ts = i * element.update_period
+                    prev_state_ts, prev_state_val = self.get_prev_state(states_dict, element, ts)
+                    next_state_ts, next_state_val = self.get_next_state(states_dict, element, ts)
+                    val = str(prev_state_val + (next_state_val - prev_state_val) * (ts - prev_state_ts) / (next_state_ts - prev_state_ts))
+                    events_list.append((ts, val))
+                    # TODO: add the correct events to the trace
+
+    def get_prev_state(self, states_dict: dict, element: ScheduleElement, ts: int) -> Tuple[int, int]:
+        states_ts: Dict[int, Tuple[int, int]] = {self.to_time(i.time): (i.min_value, i.max_value) for i in element.states}
+        if ts in states_dict.keys():
+            return ts, states_dict[ts]
+        elif ts % 1440 in states_ts:
+            # Generate a new data point and store it in the dict
+            state = states_ts[ts % 1440]
+            val = random.randint(state[0], state[1])
+            states_dict[ts] = val
+            return ts, val
+        else:
+            # Find the oldest previous state and return it
+            prev_ts = -1
+            prev_val = 0
+            changed = False
+            for state_ts in states_dict.keys():
+                if state_ts < ts and state_ts > prev_ts:
+                    prev_ts = state_ts
+                    prev_val = states_dict[state_ts]
+                    changed = True
+                if changed:
+                    return prev_ts, prev_val
+                else:
+                    # Generate a new point
+
+        return - 1, -1
+
+    def get_next_state(self, states_dict: dict, element: ScheduleElement, ts: int) -> Tuple[int, int]:
+        states_ts: Dict[int, Tuple[int, int]] = {self.to_time(i.time): (i.min_value, i.max_value) for i in element.states}
+        if ts in states_dict.keys():
+            return ts, states_dict[ts]
+        elif ts % 1440 in states_ts:
+            # Generate a new data point and store it in the dict
+            state = states_ts[ts % 1440]
+            val = random.randint(state[0], state[1])
+            states_dict[ts] = val
+            return ts, val
+        else:
+            # Find the oldest previous state and return it
+            prev_ts = -1
+            prev_val = 0
+            for state_ts in states_dict.keys():
+                if state_ts > ts and state_ts < prev_ts:
+                    prev_ts = state_ts
+                    prev_val = states_dict[state_ts]
+                return prev_ts, prev_val
+        return -1, -1
 
     def write_trace(self, overwrite: bool = False):
         "Dump the generated event trace to a file"
