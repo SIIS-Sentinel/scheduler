ac8f6fa8b163df7ecf2ce9ee7fd9b02afda5ed7b
Adrien Cosson <ad.cosson@gmail.com>
2020-06-22 19:47:23 -0400

    Fixed typo in file path

--- experiments/sensor/sensor.py
+++ experiments/sensor/sensor.py
@@ -1,4 +1,4 @@
 from scheduler.main_scheduler import Scheduler
 
-scheduler = Scheduler("./sensot_trace.txt", "./sensor_config.json")
+scheduler = Scheduler("./sensor_trace.txt", "./sensor_config.json")
 scheduler.start()
